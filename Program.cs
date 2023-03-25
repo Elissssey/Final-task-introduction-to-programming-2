95a87a2138ec0ec4cfe715111dfe90198c44bd9e
EDK <eliseev.rds@gmail.com>
2023-03-25 11:27:52 +0300

    Задали Nое

--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
 ﻿//Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
 
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введите N: ");
+        int N = int.Parse(Console.ReadLine());
+        PrintNumbers(N);
+    }
